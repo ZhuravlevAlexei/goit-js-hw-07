c97e15c31fa9432cad7858fd492c29589070b573
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2022-12-24 16:11:43 +0200

    Друга та дрібниці

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -4,46 +4,6 @@ import { galleryItems } from "./gallery-items.js";
 // console.log(galleryItems);
 
 let myGallery = document.querySelector(".gallery");
-let instanceBlb = "";
-
-function onEscape(evt) {
-  if (!basicLightbox.visible()) {
-    document.removeEventListener("keydown", onEscape);
-    return;
-  }
-  if (evt.code === "Escape") {
-    //console.log(instanceBlb);
-    if (basicLightbox.visible()) {
-      instanceBlb.close();
-      //console.log("closed!!!!");
-      document.removeEventListener("keydown", onEscape);
-      return;
-    }
-  }
-  //   console.log(evt);
-}
-
-function showModalWindow(myLink) {
-  instanceBlb = basicLightbox.create(
-    `<img width="1400" height="900" src="${myLink}"></img>`
-  );
-  //console.log(instanceBlb);
-  instanceBlb.show();
-  document.addEventListener("keydown", onEscape);
-}
-
-function galleryOnClick(evt) {
-  evt.preventDefault();
-  let targetLink = evt.target.dataset.source;
-  if (!targetLink) {
-    alert("Не знайдено посилання на зображення!");
-  } else {
-    showModalWindow(targetLink);
-  }
-  //   console.log(evt.target.dataset.source);
-}
-
-myGallery.addEventListener("click", galleryOnClick);
 
 const markup = galleryItems
   .map(
@@ -59,4 +19,10 @@ const markup = galleryItems
 
 myGallery.insertAdjacentHTML("beforeend", markup);
 
-//console.log(basicLightbox);
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  //   captionPosition: "bottom", //default = "bottom"
+  captionDelay: 250,
+});
+
+// console.log(lightbox);
